3e6a2a9f13fd71b5b83f9ab44f7329debfec6a81
Rafal <rafalmulinski@gmail.com>
2022-06-08 19:06:34 +0200

    Add first function summation

--- calkulator/main.c
+++ calkulator/main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
-
+#include "addition.h"
 int main(void) {
-printf("Compilation is compled!!");
+char action;
+double a1, a2;
+
+
+printf ("Hello USERS!!!\n");
+printf ("Enter on operation (+,-,*,/): ");
+scanf("%c", &action);
+printf("Enter two operands:\n");
+scanf("%lf %lf", &a1 ,&a2);
+switch(action)
+{
+case '+':
+    printf("The result is : %1f", summation(a1,a2));
+    break;
+
+case '-':
+    
+    break;
+
+case '*':
+    
+    break;
+
+case '/':
+    
+    break;
 
 return 0;
+}
 }
---
+++ library/addition.h
@@ -0,0 +1,16 @@
+#ifndef INC_ADDITION_H_
+#define INC_ADDITION_H_
+
+
+
+
+double summation(double a1 , double a2);
+
+
+
+
+
+
+
+#endif /* INC_ADDITION_H_ */
+
