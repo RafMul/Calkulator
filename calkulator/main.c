fcd495870cc4e3aab89ba6bfd58c89d3f4f4ef1e
Rafal <rafalmulinski@gmail.com>
2022-06-08 19:30:13 +0200

    add

--- calkulator/main.c
+++ calkulator/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
 int main(void) {
-printf("Compilation is compled!!");
+char operation;
+    double n1, n2;
+printf("Welkome USERS\n ");
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operation);
+    printf("Enter two operands: ");
+    scanf("%lf %lf",&n1, &n2);
 
+    switch(operation)
+    {
+        case '+':
+            printf("Result is %lf",summation(n1 , n2);
+            break;
+
+        case '-':
+            
+            break;
+
+        case '*':
+         
+            break;
+
+        case '/':
+         
+            break;
 return 0;
 }
