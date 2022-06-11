a4e6f853b75d54466fed417f5f917c0cf50a9abd
Rafal <rafalmulinski@gmail.com>
2022-06-11 13:21:35 +0200

    Add function subtraktion and add branch sub

---
+++ library/subtraction.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "subtraction.h"
+//funkcja sumowanie zmiennych a1 i a2
+double sub(double a1 , double a2){
+
+double sub;
+sub = a1 + a2;
+
+return sub;
+
+
+}
---
+++ library/subtraction.h
@@ -0,0 +1,16 @@
+#ifndef INC_SUBTRACTION_H_
+#define INC_SUBTRACTION_H_
+
+
+
+
+double sub(double a1 , double a2);
+
+
+
+
+
+
+
+#endif /* INC_SUBTRACTION_H_ */
+
